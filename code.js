3da59da93bdbd9999346f4eea15532667fbc0e1a
ValentinValleWD <144379302+ValentinValleWD@users.noreply.github.com>
2023-09-13 12:07:08 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 hamburgerButton = document.querySelector('.hamburger-button');
-var imagenes = ['AFTER.png', 'BEFORE.PNG'];
+var imagenes = ['w/AFTER.png', 'w/BEFORE.PNG'];
 var indice = 1;
 var indice1 = 1;
 var indice2 = 1;
